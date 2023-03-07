f0065f262e7783895e907e7280115530ba2f3c5b
GutemaG <bir13gud@gmail.com>
2023-03-07 22:52:40 +0300

    Fix issues from reviews by importing required file in person.rb and create new objects

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
 require './nameable'
+require './capitalize_decorator'
+require './trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
@@ -26,3 +28,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
